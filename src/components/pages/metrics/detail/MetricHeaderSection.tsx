f9c4f4b9e86c3d7c84a7c90ec73ef3a5defb9d99
dimaszisby <dimaszisby@gmail.com>
2025-07-23 19:52:57 +0700

    update: [component - metric] adding update functionality for metric core

--- src/components/pages/metrics/detail/MetricHeaderSection.tsx
+++ src/components/pages/metrics/detail/MetricHeaderSection.tsx
@@ -1,9 +1,10 @@
 // MetricHeaderSection.tsx
 
+import React, { useState } from "react";
+
 // components
 import { DataLabel } from "@/src/components/ui/DataLabel";
 import OverlineLabel from "@/src/components/ui/OverlineLabel";
-import { safeLabel } from "@/utils/helpers/labelHelper";
 import { PencilSimple } from "phosphor-react";
 
 // types
@@ -11,79 +12,101 @@ import { MetricCore } from "@/src/features/metrics/types";
 
 // utlis
 import { formatDate } from "@/utils/helpers/dateHelper";
+import { safeLabel } from "@/utils/helpers/labelHelper";
+import MetricForm from "../MetricForm";
 
 /**
  * Metric detail header card, matching Figma layout.
+ * A static view components, only updated when the parent view refreshed.
  */
 
 interface MetricHeaderSectionProps {
   metric: MetricCore;
-  onEditMetric: () => void;
 }
 
 const MetricHeaderSection: React.FC<MetricHeaderSectionProps> = ({
   metric,
 }) => {
-  // Developer Note: Metric Category component is wIP
+  // TODO - Developer Note: Metric Category component is WIP
+
+  // States
+  const [modalOpen, setModalOpen] = useState(false);
+
+  // Handlers
+  const handleUpdateMetric = () => {
+    setModalOpen(true);
+  };
 
   return (
-    <section className="w-full bg-white rounded-2xl shadow p-6 relative">
-      {/* Edit Button */}
-      <button
-        className="absolute top-5 right-5 rounded-full p-2 bg-[#F4C3CD] text-[#C76576] hover:bg-[#E897A3] transition"
-        aria-label="Edit Metric"
-      >
-        <PencilSimple size={22} />
-      </button>
+    <>
+      <MetricForm
+        metricId={metric.id}
+        open={modalOpen}
+        onClose={() => setModalOpen(false)}
+        initialMetric={metric}
+      />
 
-      {/* Metric Title */}
-      <div className="mb-4">
-        <OverlineLabel text="Metric Name" />
-        <div className="font-bold text-xl text-gray-800">{metric?.name}</div>
-      </div>
+      <section className="w-full bg-white rounded-2xl shadow p-6 relative">
+        {/* Edit Button */}
+        <button
+          className="absolute top-5 right-5 rounded-full p-2 bg-[#F4C3CD] text-[#C76576] hover:bg-[#E897A3] transition"
+          aria-label="Edit Metric"
+          onClick={handleUpdateMetric}
+        >
+          <PencilSimple size={22} />
+        </button>
 
-      {/* Metric Unit, Category, Visibility */}
-      <div className="flex flex-wrap gap-8 mb-4">
-        <DataLabel
-          label="Default Unit"
-          value={safeLabel(metric?.defaultUnit, "Not Set")}
-        />
-
-        <div>
-          <OverlineLabel text="Category" />
+        {/* Metric Title */}
+        <div className="mb-4">
+          <OverlineLabel text="Metric Name" />
+          <div className="font-bold text-xl text-gray-800">{metric?.name}</div>
         </div>
-        <div>
-          <OverlineLabel text="Visibility" />
-          <div className={metric.isPublic ? "text-green-600" : "text-red-600"}>
-            {metric.isPublic ? "Public" : "Private"}
+
+        {/* Metric Unit, Category, Visibility */}
+        <div className="flex flex-wrap gap-8 mb-4">
+          <DataLabel
+            label="Default Unit"
+            value={safeLabel(metric?.defaultUnit, "Not Set")}
+          />
+
+          <div>
+            <OverlineLabel text="Category" />
+          </div>
+          <div>
+            <OverlineLabel text="Visibility" />
+            <div
+              className={metric.isPublic ? "text-green-600" : "text-red-600"}
+            >
+              {metric.isPublic ? "Public" : "Private"}
+            </div>
           </div>
         </div>
-      </div>
 
-      {/* Description */}
-      <div className="mb-3">
-        <OverlineLabel text="Description" />
-        <div className="text-gray-600">
-          {metric.description || (
-            <span className="italic text-gray-400">
-              No description provided.
-            </span>
-          )}
+        {/* Description */}
+        <div className="mb-3">
+          <OverlineLabel text="Description" />
+          <div className="text-gray-600">
+            {metric.description || (
+              <span className="italic text-gray-400">
+                No description provided.
+              </span>
+            )}
+          </div>
         </div>
-      </div>
 
-      {/* Created/Updated At */}
-      <div className="flex gap-6 text-xs text-gray-400 mt-3">
-        <span>
-          Created at&nbsp;
-          {formatDate(metric?.createdAt, true)}
-        </span>
-        <span>
-          Updated at&nbsp;
-          {formatDate(metric?.updatedAt, true)}
-        </span>
-      </div>
-    </section>
+        {/* Created/Updated At */}
+        <div className="flex gap-6 text-xs text-gray-400 mt-3">
+          <span>
+            Created at&nbsp;
+            {formatDate(metric?.createdAt, true)}
+          </span>
+          <span>
+            Updated at&nbsp;
+            {formatDate(metric?.updatedAt, true)}
+          </span>
+        </div>
+      </section>
+    </>
   );
 };
 
