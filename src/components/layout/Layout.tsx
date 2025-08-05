a7e63d0caa7e2fbdba69c809530a7dfb6f01553d
dimaszisby <dimaszisby@gmail.com>
2025-08-05 16:45:17 +0700

    update: [component - layout] updating layout styling

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -12,7 +12,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
       <Sidebar />
 
       {/* ✅ Adjust main content layout to prevent overflow issues */}
-      <main className="bg-red-100 flex-1 min-h-screen lg:ml-64 p-6 overflow-x-hidden">
+      <main className="pt-20 sm:pt-20 lg:pt-4 bg-red-100 flex-1 min-h-screen sm:p-0 lg:p-6 overflow-x-hidden lg:ml-[272px]">
         {children}
       </main>
     </div>
