47cc1082603b884c004de9926b109bb762500ad0
dimaszisby <dimaszisby@gmail.com>
2025-08-28 14:55:55 +0700

    update: [features - metrics] types => removing metric-core and replacing with vm

--- src/features/metrics/types.ts
+++ src/features/metrics/types.ts
@@ -1,28 +1,13 @@
 import { createMetricSchema } from "@/src/types/api/zod-metric.schema";
-import { UserMetricDetailResponseDTO } from "@/src/types/dtos/metric.dto";
 import { z } from "zod";
 import { ServerSortBy, SortOrder } from "./sort";
 
-// Pick only core properties
-export type MetricCore = Pick<
-  UserMetricDetailResponseDTO,
-  | "id"
-  | "userId"
-  | "categoryId"
-  | "originalMetricId"
-  | "name"
-  | "description"
-  | "defaultUnit"
-  | "isPublic"
-  | "createdAt"
-  | "updatedAt"
->;
-
-// * ========== Form
+// ===== Form =====
 export const metricFormSchema = createMetricSchema.shape.body;
 export type MetricFormInputs = z.infer<typeof metricFormSchema>;
 
-// * ========== API Includes and Params
+// ===== API Includes and Params =====
+
 // MetricExtended Opts
 export type IncludeKey = "settings" | "category" | "logs"; // For extended metric details
 export type ListOptions = { enabled?: boolean; staleTime?: number };
